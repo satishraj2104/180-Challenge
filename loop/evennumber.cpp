8c3f1fedf75c64d1ea537666ffc3ac3427c7e215
satish raj <satishraj2104@gmail.com>
2026-03-30 17:48:38 +0530

    my first commit loop task

---
+++ loop/evennumber.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+	for(int i=1;i<=50;i++){
+		if(i%2==0){
+			std::cout<<i<<" ";
+		}
+	}
+	return 0;
+}
---
+++ loop/reversenumber.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+	for(int i=-10;i<=-1;i++){
+		std::cout<<i<<" ";
+	}
+	std::cout<<"\n";
+	return 0;
+}
